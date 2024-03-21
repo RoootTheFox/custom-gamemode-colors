39fe588e2033488623f91df812ab032c70fbf778
rooot <hey@rooot.gay>
2024-03-21 23:44:32 +0100

    ?
    Signed-off-by: rooot <hey@rooot.gay>

--- src/main.cpp
+++ src/main.cpp
@@ -129,7 +129,7 @@ $on_mod(Loaded) {
 	settings->m_defaultColor = gameManager->m_playerColor;
 	settings->m_defaultColor2 = gameManager->m_playerColor2;
 #ifdef GEODE_MACOS
-	static_assert(GEODE_COMP_GD_VERSION == 22000, "Mod needs to target 2.200 on mac"); // ty cvolton for this (misc bugfixes)
+	//static_assert(GEODE_COMP_GD_VERSION == 22000, "Mod needs to target 2.200 on mac"); // ty cvolton for this (misc bugfixes)
 
 	// thank you dankmeme for telling me where flashPlayer is inlined <3
 	log::info("patching out inlined flashPlayer calls !");
