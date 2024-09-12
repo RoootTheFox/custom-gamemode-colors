83909d9be988da3945d52befddf800287beb2546
rooot <hey@rooot.gay>
2024-09-13 00:25:14 +0200

    fix: hacky workaround for SDI overriding colors
    Signed-off-by: rooot <hey@rooot.gay>

---
+++ src/menulayer_hooks.cpp
@@ -0,0 +1,31 @@
+#include <Geode/modify/MenuLayer.hpp>
+
+#include "settings.hpp"
+using namespace geode::prelude;
+
+class $modify(MenuLayer) {
+    struct Fields {
+        bool overriden = false;
+    };
+
+    bool init() {
+        // this is **TEMPORARY** - this will be removed once SDI doesn't hook setColor anymore (lmao)
+        if (Loader::get()->isModLoaded("weebify.separate_dual_icons") && !m_fields->overriden) {            
+            for (Hook* h : Loader::get()->getLoadedMod("weebify.separate_dual_icons")->getHooks()) {
+                auto function_name = h->getDisplayName();
+                if (function_name == "PlayerObject::setColor" || function_name == "PlayerObject::setSecondColor") {
+                    auto res = h->disable();
+                    if (res.isOk()) {
+                        log::info("sdi hook for {} disabled !!", function_name);
+                    } else {
+                        log::error("failed to disable sdi hook for {}: {}", function_name, res.error());
+                    }
+                }
+            }
+
+            m_fields->overriden = true;
+        }
+
+        return MenuLayer::init();
+    }
+};
